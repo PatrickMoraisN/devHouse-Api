ed98857bcfa14125bb8a6bb36610c8f5b59fddeb
Patrick Morais <ppternunes@gmail.com>
2021-10-10 19:17:36 -0300

    fix reserve controller

--- src/controllers/ReserveController.js
+++ src/controllers/ReserveController.js
@@ -14,12 +14,12 @@ class ReserveController {
       return res.status(400).json({error: "House not found"});
     }
 
-    if (house.status !== true) {
-      return res.status(401).json({error: "House not avaiable"});
-    }
-
     if (String(house.user) === String(user_id)) {
       return res.status(401).json({error: "Not Authorized"});
+    }
+    
+    if (house.status !== true) {
+      return res.status(401).json({error: "House not avaiable"});
     }
 
     const reserve = await Reserve.create({
