ae28aaa581723a4f27ebe3dca54b1f331bad3de0
Patrick Morais <ppternunes@gmail.com>
2021-10-10 09:39:20 -0300

    add multer to routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import HouseController from './controllers/HouseController';
 import SessionController from './controllers/SessionController';
+import multer from 'multer';
+import uploadConfig from './config/upload';
 
 const routes = new Router();
+const upload = multer(uploadConfig);
 
 routes.get('/', SessionController.index);
 routes.post('/sessions', SessionController.store);
-routes.post('/houses', HouseController.store);
+routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 
 export default routes;
