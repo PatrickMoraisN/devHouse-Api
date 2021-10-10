a871fffc00bd51c9661bbe0ea58487b29fc10e37
Patrick Morais <ppternunes@gmail.com>
2021-10-09 23:34:55 -0300

    fix name project / refactor connection

--- src/app.js
+++ src/app.js
@@ -29,24 +29,3 @@ class App {
 }
 
 export default new App().server;
-
-/*
-const { MongoClient } = require('mongodb');
-
-const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-
-const DB_NAME = 'Cookmaster';
-
-const connection = () => MongoClient
-  .connect(MONGO_DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((conn) => conn.db(DB_NAME))
-  .catch((err) => {
-    console.error(err);
-    process.exit();
-  });
-
-module.exports = connection; 
-*/
