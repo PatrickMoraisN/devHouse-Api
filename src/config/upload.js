7e599a15979c03f10ea4f01ac9780b8a9d72eb2b
Patrick Morais <ppternunes@gmail.com>
2021-10-10 18:37:22 -0300

    add dashboard routes - controller

--- src/config/upload.js
+++ src/config/upload.js
@@ -4,7 +4,7 @@ import path from 'path';
 export default {
   storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
-    filename: (req, file, cb) => {
+    filename: (_req, file, cb) => {
       const ext = path.extname(file.originalname);
       const name = path.basename(file.originalname, ext);
 
