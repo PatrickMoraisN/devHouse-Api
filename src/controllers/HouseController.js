73acc7782ddf3f98d9864815aa19f1d6cc07637c
Patrick Morais <ppternunes@gmail.com>
2021-10-10 09:47:53 -0300

    House Controller

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,7 +1,22 @@
+import House from '../models/House';
+
 class HouseController {
 
   async store(req, res) {
-    return res.json({ok: true})
+    const { filename } = req.file;
+    const { description, location, price, status } = req.body;
+    const { user_id } = req.headers;
+
+    const house = await House.create({
+      user: user_id,
+      thumbnail: filename,
+      description,
+      location,
+      price,
+      status
+    })
+
+    return res.json(house)
   }
 }
 
