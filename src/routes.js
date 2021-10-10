ac66ccf78da2479ee631e1cfb95edd1ea56902e9
Patrick Morais <ppternunes@gmail.com>
2021-10-10 09:28:02 -0300

    add houses route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import HouseController from './controllers/HouseController';
 import SessionController from './controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/', SessionController.index);
 routes.post('/sessions', SessionController.store);
+routes.post('/houses', HouseController.store);
 
 export default routes;
