3f9491d07e96697948eb075e034962b6b599320f
Patrick Morais <ppternunes@gmail.com>
2021-10-10 22:53:12 -0300

    add yup validation email

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,4 +1,3 @@
-
 /*
 index, show, update, destroy, store
 
@@ -9,25 +8,33 @@ destroy - deletar sessao
 store - criar sessao
 */
 
-import User from '../models/User';
+import User from "../models/User";
+import * as Yup from "yup";
 
 class SessionController {
-
   async store(req, res) {
     try {
+      const schema = Yup.object().shape({
+        email: Yup.string().email().required(),
+      });
       const { email } = req.body;
+
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ error: "Email validation error" });
+      }
+
       let user = await User.findOne({ email });
       if (!user) {
         user = await User.create({ email });
       }
-      return res.json(user)
+      return res.json(user);
     } catch (err) {
-      console.log(err.message)
+      console.log(err.message);
     }
   }
 
   index(req, res) {
-    return res.json('funfando')
+    return res.json("funfando");
   }
 }
 
