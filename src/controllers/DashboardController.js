1302d30f358ac9a790cb6f9700e7f0741c5ed0f9
Patrick Morais <ppternunes@gmail.com>
2021-10-10 23:01:45 -0300

    prettier

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,16 +1,16 @@
-import House from '../models/House';
-import User from '../models/User';
+import House from "../models/House";
+import User from "../models/User";
 
 class DashboardController {
   async show(req, res) {
     const { user_id } = req.headers;
 
     const user = await User.findById(user_id);
-    const houses = await House.find({user: user_id})
+    const houses = await House.find({ user: user_id });
     if (!user) {
-      return res.status(401).json({error: "not authorized"});
+      return res.status(401).json({ error: "not authorized" });
     }
-    return res.json(houses)
+    return res.json(houses);
   }
 }
 
--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,9 +1,8 @@
-import House from '../models/House';
-import User from '../models/User';
-import * as Yup from 'yup';
+import House from "../models/House";
+import User from "../models/User";
+import * as Yup from "yup";
 
 class HouseController {
-
   async index(req, res) {
     const { status } = req.query;
 
@@ -24,8 +23,8 @@ class HouseController {
     const { description, location, price, status } = req.body;
     const { user_id } = req.headers;
 
-    if(!(await schema.isValid(req.body))) {
-      return res.status(400).json({error: "Validation failed!"})
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation failed!" });
     }
 
     const house = await House.create({
@@ -34,44 +33,47 @@ class HouseController {
       description,
       location,
       price,
-      status
-    })
+      status,
+    });
 
-    return res.json(house)
+    return res.json(house);
   }
 
-  async update(req,res) {
+  async update(req, res) {
     const schema = Yup.object().shape({
       description: Yup.string().required(),
       price: Yup.number().required(),
       location: Yup.string().required(),
       status: Yup.boolean().required(),
     });
-  
+
     const { filename } = req.file;
     const { house_id } = req.params;
     const { description, location, price, status } = req.body;
     const { user_id } = req.headers;
 
-    if(!(await schema.isValid(req.body))) {
-      return res.status(400).json({error: "Validation failed!"})
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation failed!" });
     }
 
     const user = await User.findById(user_id);
     const house = await House.findById(house_id);
 
-    if(!user || !house || String(user._id) !== String(house.user)) {
-      return res.status(401).json({ error: "Not Authorized!"});
+    if (!user || !house || String(user._id) !== String(house.user)) {
+      return res.status(401).json({ error: "Not Authorized!" });
     }
 
-    await House.updateOne({ _id: house_id }, {
-      user: user_id,
-      thumbnail: filename,
-      description,
-      location,
-      price,
-      status
-    })
+    await House.updateOne(
+      { _id: house_id },
+      {
+        user: user_id,
+        thumbnail: filename,
+        description,
+        location,
+        price,
+        status,
+      }
+    );
 
     return res.send();
   }
@@ -84,13 +86,13 @@ class HouseController {
     const house = await House.findById(house_id);
 
     console.log(user);
-    if(!user || !house || String(user._id) !== String(house.user)) {
-      return res.status(401).json({ error: "Not Authorized!"});
+    if (!user || !house || String(user._id) !== String(house.user)) {
+      return res.status(401).json({ error: "Not Authorized!" });
     }
 
-    await House.findByIdAndDelete({ _id: house_id })
+    await House.findByIdAndDelete({ _id: house_id });
 
-    return res.json({success: "House deleted!"})
+    return res.json({ success: "House deleted!" });
   }
 }
 
